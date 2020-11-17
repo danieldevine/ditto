2c5106cc9387f6e35a50009c8ce0d99f21ba1248
Dan Devine <dan@maverick-intl.com>
2020-11-17 10:54:15 +0000

    deploy

---
+++ precache-manifest.d14fe980640ea39ea5bdb64bee61bee5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f460c6e2f103dd443365",
+    "url": "/ditto/css/app.3f16034f.css"
+  },
+  {
+    "revision": "f97e3630dc2b41fcb46759613ff2a288",
+    "url": "/ditto/data/prompts.json"
+  },
+  {
+    "revision": "9748297caa9cbf64707b0bdad966659a",
+    "url": "/ditto/index.html"
+  },
+  {
+    "revision": "cc3e5b42f63dc732cbe4",
+    "url": "/ditto/js/about.e9359377.js"
+  },
+  {
+    "revision": "f460c6e2f103dd443365",
+    "url": "/ditto/js/app.c4ac97c5.js"
+  },
+  {
+    "revision": "dc92d772f895fbc0bf31",
+    "url": "/ditto/js/chunk-vendors.6cd7920e.js"
+  },
+  {
+    "revision": "f8b6f87d974fb0289d2002ee648d885b",
+    "url": "/ditto/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ditto/robots.txt"
+  }
+]);
